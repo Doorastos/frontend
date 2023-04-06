9ee9b3b74588bdb259579dce23d5ad87be4c24b5
podpison <au25557@yandex.ru>
2023-04-06 16:32:56 +0500

    0.0.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,48 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
+import localFont from '@next/font/local';
+
+const robodronFont = localFont({
+  src: [
+    {
+      path: './../public/fonts/robodron/Robodron.ttf',
+      weight: '400',
+    },
+    {
+      path: './../public/fonts/robodron/Robodron-medium.ttf',
+      weight: '500',
+    },
+    {
+      path: './../public/fonts/robodron/Robodron-bold.ttf',
+      weight: '700',
+    },
+  ],
+  variable: '--font-robodron'
+});
+const sfProDisplayFont = localFont({
+  src: [
+    {
+      path: './../public/fonts/sfProDisplay/sf-pro-text-light.ttf',
+      weight: '300',
+    },
+    {
+      path: './../public/fonts/sfProDisplay/sf-pro-text.ttf',
+      weight: '400',
+    },
+    {
+      path: './../public/fonts/sfProDisplay/sf-pro-text-medium.ttf',
+      weight: '500',
+    },
+    {
+      path: './../public/fonts/sfProDisplay/sf-pro-text-bold.ttf',
+      weight: '700',
+    },
+  ],
+  variable: '--font-sfProDisplay'
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className={`${robodronFont.variable} ${sfProDisplayFont.variable} font-sans`}>
+    <Component {...pageProps} />
+  </div> 
 }
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        robodron: ['var(--font-robodron)'],
+        sfProDisplay: ['var(--font-sfProDisplay)'],
+      },
+      colors: {
+        'white': '#FEFEFE',
+        'black': '#010101',
+        'bg': '#FAFAFA',
+        'grey1': '#222222',
+        'grey2': '#727272',
+        'grey3': '#F2F2F2',
+        'grey4': '#C7C7C7',
+        'blue1': '#0222A5',
+        'blue2': '#00197E',
+        'line': '#E6E6E6',
+        'red': '#D62D30'
+      },
+      boxShadow: {
+        'common': '0px 4px 30px 9px rgba(2, 34, 165, 0.05)'
+      }
+    },
+  },
+  plugins: [],
+}
+
